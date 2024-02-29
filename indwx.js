90ea3efbbef2da9550e799b979423cf99cf11b8a
xodnd0507 <24457545yong@gmail.com>
2024-02-29 15:40:42 +0900

    feat : Create multiplication table printer

--- indwx.js
+++ indwx.js
@@ -51,5 +51,8 @@
 
 
     for (var i = 2; i < 10; i++){
-        console.log(i);
-    }
+        console.log(i+'단');
+        for (var j = 1; j < 10; j++){
+            console.log(i + '*' + j +'='+ i*j);
+        }
+    } 
